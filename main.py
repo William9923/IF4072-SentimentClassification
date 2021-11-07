6d54cfa963eb670b9cd14f54736bcf7cb177af06
William9923 <williamong1400@gmail.com>
2021-11-07 18:38:27 +0700

    improve: full restructure for easier implementation

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
 # ========================================================
 # Scripts for initialize sentiment classification pipeline
 # ========================================================
+
+# SentimenAnalyzer class definition...
+
+# inject... disini pokoknya semuanya
+
+# Need to define experiment scopes :
+# Changing parameter:
+
+# DataLoader:
+# SentimentClfLoader : ...
+
+# == P0 Scenario ==
+# Preprocessor: 
+# W2VPreprocessor
+# VectorSpacePreprocessor
+
+# == P1 Scenario ==
+
+# Word Representation + Parameter:  
+# VectorSpace : 
+# FastText : 
+# BERT / Glove (pick 1) 
+
+# == P2 Scenario ==
+# Model:
+# Default1 (Basic only -> kea di model yoga aja)
+# Default2 (LGBM)
+# SVM -> Traditional Machine Learning
+# LSTM + Attention
+# CNN
+
+# == P3 Scenario ==
+# Finding Best Parameter for the selected model...
